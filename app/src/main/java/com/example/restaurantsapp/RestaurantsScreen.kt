54ae51d03634b2b2305fb3032119c1891974caf6
roblesdotdev <httpsnip@gmail.com>
2023-11-18 18:44:47 -0300

    hoist state to viewModel

--- app/src/main/java/com/example/restaurantsapp/RestaurantsScreen.kt
+++ app/src/main/java/com/example/restaurantsapp/RestaurantsScreen.kt
@@ -32,22 +32,15 @@ import com.example.restaurantsapp.ui.theme.RestaurantsAppTheme
 @Composable
 fun RestaurantsScreen() {
     val viewModel: RestaurantsViewModel = viewModel()
-    val state: MutableState<List<Restaurant>> = remember {
-        mutableStateOf(viewModel.getRestaurants())
-    }
     LazyColumn(
         contentPadding = PaddingValues(
             vertical = 8.dp,
             horizontal = 8.dp,
         )
     ) {
-        items(state.value) { restaurant ->
+        items(viewModel.state.value) { restaurant ->
             RestaurantItem(item = restaurant) { id ->
-                val restaurants = state.value.toMutableList()
-                val itemIndex = restaurants.indexOfFirst { it.id == id }
-                val item = restaurants[itemIndex]
-                restaurants[itemIndex] = item.copy(isFavorite = !item.isFavorite)
-                state.value = restaurants
+                viewModel.toggleFavorite(id)
             }
         }
     }
